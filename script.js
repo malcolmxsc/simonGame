3f521136270ce2663e4d58f0cdd531468dfe1130
malcolm griffinE <malcolmxsc@gmail.com>
2023-06-27 16:41:05 -0700

    create initial game board

---
+++ script.js
@@ -0,0 +1,25 @@
+ /*----- constants -----*/
+ const playTime = 1000;
+ const PauseTime = 500;
+ const playWaitTime = 1000;
+
+
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
+
+
+  function startGame(){
+    gameStart = true;
+
+
+
+  }
